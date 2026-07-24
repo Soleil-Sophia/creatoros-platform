ededc3415e9ddc90a3e526e517a21ecca8ef507c
sslv <119299074+Soleil-Sophia@users.noreply.github.com>
2026-07-24 02:51:32 +0200

    feat: export focus history APIs

--- src/core/decision-engine/index.ts
+++ src/core/decision-engine/index.ts
@@ -6,88 +6,22 @@ export {
   markRecommendationObserved,
   submitRecommendation,
 } from './engine';
-export {
-  applyApprovedBrandToneRecommendation,
-  previewBrandToneCanonicalApply,
-} from './brandosApply';
-export {
-  createBrandOSRevisionRecord,
-  listBrandOSRevisionRecords,
-  saveBrandOSRevisionRecord,
-} from './brandosRevisionHistory';
-export {
-  createImplementationRecord,
-  createObservationRecord,
-  listImplementationRecords,
-  listObservationRecords,
-  saveImplementationRecord,
-  saveObservationRecord,
-} from './outcomes';
+export { applyApprovedBrandToneRecommendation, previewBrandToneCanonicalApply } from './brandosApply';
+export { createBrandOSRevisionRecord, listBrandOSRevisionRecords, saveBrandOSRevisionRecord } from './brandosRevisionHistory';
+export { createImplementationRecord, createObservationRecord, listImplementationRecords, listObservationRecords, saveImplementationRecord, saveObservationRecord } from './outcomes';
 export { createIntelligenceLearningRecommendation } from './intelligenceLearning';
-export {
-  aggregateObservationPattern,
-  createPatternLearningRecommendation,
-  hasOpenLearningRecommendation,
-} from './intelligencePatterns';
-export {
-  getDecisionOperationalMetadata,
-  isOperationallyOverdue,
-  listDecisionOperationalMetadata,
-  saveDecisionOperationalMetadata,
-} from './operationalMetadata';
-export {
-  createDecisionDependency,
-  deleteDecisionDependency,
-  getBlockedDependents,
-  getBlockingDependencies,
-  getUnresolvedBlockingDependencies,
-  isDependencyResolved,
-  listDecisionDependencies,
-  saveDecisionDependency,
-} from './dependencies';
+export { aggregateObservationPattern, createPatternLearningRecommendation, hasOpenLearningRecommendation } from './intelligencePatterns';
+export { getDecisionOperationalMetadata, isOperationallyOverdue, listDecisionOperationalMetadata, saveDecisionOperationalMetadata } from './operationalMetadata';
+export { createDecisionDependency, deleteDecisionDependency, getBlockedDependents, getBlockingDependencies, getUnresolvedBlockingDependencies, isDependencyResolved, listDecisionDependencies, saveDecisionDependency } from './dependencies';
 export { deriveDecisionAttentionSignals } from './attention';
-export {
-  acknowledgeDecisionAttentionSignal,
-  clearDecisionAttentionState,
-  getDecisionAttentionState,
-  isDecisionAttentionSignalVisible,
-  listDecisionAttentionStates,
-  snoozeDecisionAttentionSignal,
-} from './attentionState';
+export { acknowledgeDecisionAttentionSignal, clearDecisionAttentionState, getDecisionAttentionState, isDecisionAttentionSignalVisible, listDecisionAttentionStates, snoozeDecisionAttentionSignal } from './attentionState';
 export { deriveDecisionDailyFocus } from './dailyFocus';
-export {
-  clearDecisionFocusPlan,
-  createFocusPlanItems,
-  getDecisionFocusPlan,
-  saveDecisionFocusPlan,
-} from './focusPlan';
-export {
-  closeDecisionFocusDay,
-  getDecisionFocusDayClose,
-  getDecisionFocusExecutionItem,
-  listDecisionFocusExecutionItems,
-  updateDecisionFocusExecutionItem,
-} from './focusExecution';
+export { clearDecisionFocusPlan, createFocusPlanItems, getDecisionFocusPlan, listDecisionFocusPlans, saveDecisionFocusPlan } from './focusPlan';
+export { closeDecisionFocusDay, getDecisionFocusDayClose, getDecisionFocusExecutionItem, listAllDecisionFocusExecutionItems, listDecisionFocusDayCloses, listDecisionFocusExecutionItems, updateDecisionFocusExecutionItem } from './focusExecution';
+export { deriveDecisionFocusHistory, summarizeDecisionFocusHistory } from './focusHistory';
 export { createBrandToneRecommendation } from './factories';
-export {
-  getRecommendation,
-  listDecisionRecords,
-  listRecommendations,
-  saveDecisionRecord,
-  saveRecommendation,
-} from './storage';
-export type {
-  DecisionAction,
-  DecisionConfidence,
-  DecisionEvidence,
-  DecisionHistoryEntry,
-  DecisionOrigin,
-  DecisionRecord,
-  DecisionTargetOS,
-  PlatformRecommendation,
-  RecommendationChange,
-  RecommendationStatus,
-} from './types';
+export { getRecommendation, listDecisionRecords, listRecommendations, saveDecisionRecord, saveRecommendation } from './storage';
+export type { DecisionAction, DecisionConfidence, DecisionEvidence, DecisionHistoryEntry, DecisionOrigin, DecisionRecord, DecisionTargetOS, PlatformRecommendation, RecommendationChange, RecommendationStatus } from './types';
 export type { BrandToneRecommendationValue } from './factories';
 export type { BrandOSCanonicalPreview } from './brandosApply';
 export type { BrandOSRevisionRecord } from './brandosRevisionHistory';
@@ -101,3 +35,4 @@ export type { AttentionDisposition, DecisionAttentionState } from './attentionSt
 export type { DecisionDailyFocusItem } from './dailyFocus';
 export type { DecisionFocusPlan, DecisionFocusPlanItem } from './focusPlan';
 export type { DecisionFocusDayClose, DecisionFocusExecutionItem, FocusExecutionStatus } from './focusExecution';
+export type { DecisionFocusHistoryDay, DecisionFocusHistoryItem, DecisionFocusHistorySummary } from './focusHistory';
